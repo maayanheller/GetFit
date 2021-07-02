3ebe8de9ac0dd7ea66fb6ffeeb19929101683016
maayanheller <maayan.heller@gmail.com>
2021-07-02 22:10:59 +0300

    Added some more validation features to login.

--- GetFit/jsLoginPage.js
+++ GetFit/jsLoginPage.js
@@ -5,12 +5,12 @@
     name = document.getElementById(nameId);
     pass = document.getElementById(password);
 
-    if (name.value == "") {
+    if (name.value.trim() == "") {
         alert("Please provide your name");
         return false;
     }
 
-    if (pass.value == "") {
+    if (pass.value.trim() == "") {
         alert("Please provide password");
         return false;
     }
